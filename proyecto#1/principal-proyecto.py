27d19fa68461efeb855bb25cbc39687b68b608f9
LuisAlejandroAbreo <alejandro.abreo39@gmail.com>
2026-02-14 23:29:59 -0500

    ejercicios

--- proyecto#1/principal-proyecto.py
+++ proyecto#1/principal-proyecto.py
@@ -12,64 +12,52 @@ def menu():
     datos1 = cargar_datos1()
     datos_prestamos = cargar_datos2()
     solicitudes = {}
-    while True:
-        print("""
-\nHERRAMIENTAS
-1. Gestión de herramientas
-2. Gestión de usuarios
-3. Gestión de préstamos
-4. Consultas y reportes
-5. Iniciar sesión
-6. Salir del programa
---------------------------------
-""")
-        opcion = input("Seleccione una opción: ")
-        
-        if opcion == "1":
-            gestion_herramientas1(datos)
-        elif opcion == "2":
-            gestion_usuarios1(datos1)
-        elif opcion == "3":
-            gestion_prestamos(datos, datos1, datos_prestamos)
-        elif opcion == "4":
-            menu_consultas(datos, datos1, datos_prestamos)
-        elif opcion == "5":
-            usuario_actual = login(datos1)
-            if usuario_actual:
-                tipo = datos1[usuario_actual]["Tipo"]
-            if tipo == "Administrador":
-                while True:
-                    print("""
+
+    usuario_actual = login(datos1)
+    if usuario_actual:
+        tipo = datos1[usuario_actual]["Tipo"]
+    if tipo == "Administrador":
+        while True:
+            print("""
 1. Aprobar solicitudes
-2. Volver""")
-                    op_admin = input("Seleccione opción: ")
+2. Gestión de herramientas
+3. Gestión de usuarios
+4. Gestión de préstamos
+5. Consultas y reportes
+6. Salir del programa""")
+            op_admin = input("Seleccione opción: ")
 
-                    if op_admin == "1":
-                        aprobar_solicitudes(solicitudes, datos, datos_prestamos)
-                    elif op_admin == "2":
-                        break
-
-            else: 
-                while True:
-                    print("""
+            if op_admin == "1":
+                aprobar_solicitudes(solicitudes, datos, datos_prestamos)
+            elif  op_admin == "2":
+                gestion_herramientas1(datos)
+            elif op_admin == "3":
+                gestion_usuarios1(datos1)
+            elif op_admin == "4":
+                gestion_prestamos(datos, datos1, datos_prestamos)
+            elif op_admin == "5":
+                menu_consultas(datos, datos1, datos_prestamos)
+            elif op_admin == "6":
+                print("Salida del programa. Hasta luego!")
+                break
+            else:
+                print("Opción inválida. Intente de nuevo")
+            input("Presione cualquier tecla para continuar ....")
+    if tipo == "Residente":
+        while True:
+            print("""
 1. Consultar herramientas
 2. Solicitar herramienta
-3. Volver""")
-                    op_user = input("Seleccione opción: ")
+3. Salir del programa""")
+            op_user = input("Seleccione opción: ")
 
-                    if op_user == "1":
-                                consultar_herramientas(datos, datos_prestamos)
-                    elif op_user == "2":
-                                solicitar_herramienta(usuario_actual, solicitudes, datos)
-                    elif op_user == "3":
-                        break 
-             
-        elif opcion == "6":
-            print("Salida del programa. Hasta luego!")
-            break
-        else:
-            print("Opción inválida. Intente de nuevo")
-        input("Presione cualquier tecla para continuar ....")
-
-
+            if op_user == "1":
+                consultar_herramientas(datos, datos_prestamos)
+            elif op_user == "2":
+                solicitar_herramienta(usuario_actual, solicitudes, datos)
+            elif op_user == "3":
+                break     
+            else:
+                print("Opción inválida. Intente de nuevo")
+            input("Presione cualquier tecla para continuar ....")
 menu()
